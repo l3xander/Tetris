e42af42e0e0f3cf999fbda1bb107f66f65663739
lucibytes <lucian@bitstorm.org>
2022-10-05 16:03:08 +0200

    Add project files.

---
+++ Tetris/Block.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+internal class Block
+{
+
+}
+
+class Block1 : Block
+{
+
+}
+
+class Block2 : Block
+{
+
+}
+
+class Block3 : Block
+{
+
+}
+
+class Block4 : Block
+{
+
+}
+
+class Block5 : Block
+{
+
+}
+
+class Block6 : Block
+{
+
+}
+
+class Block7 : Block
+{
+
+}
